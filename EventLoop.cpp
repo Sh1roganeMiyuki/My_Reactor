0d1730bcea8ecc5967b3c16a0196abfb8866b104
Sh1roganeMiyuki <3182996786@qq.com>
2025-11-30 17:27:20 +0800

    EraseSubThread

--- EventLoop.cpp
+++ EventLoop.cpp
@@ -1,18 +1,75 @@
 #include "EventLoop.h"
 #include "Channel.h"  
 #include "Epoller.h"
+#include "sys/eventfd.h"
 class Epoller;
 class Channel;
-EventLoop::EventLoop() : server_epoller(std::make_unique<Epoller>()) {
-    //server_epoller = std::make_unique<Epoller>();
+
+
+int createEventfd() {
+    int evtfd = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+    if (evtfd < 0) {
+        perror("Failed in eventfd");
+        exit(1);
+    }
+    return evtfd;
+}
+
+// EventLoop::EventLoop() 
+//       : server_epoller(std::make_unique<Epoller>()),
+//         looping_(false),
+//         quit_(false),
+//         thread_id_(std::this_thread::get_id()),
+//         wakeup_fd_(createEventfd()),
+//         wakeup_channel_(std::make_unique<Channel>(wakeup_fd_)), 
+//         calling_pending_functors_(false) {
+//     wakeup_channel_->setReadCallback([this]() { this->handle_read(); });
+//     wakeup_channel_->enableReading();
+// }
+EventLoop::EventLoop() 
+      : server_epoller(std::make_unique<Epoller>()) {
 }
 EventLoop::~EventLoop() {
+    // wakeup_channel_->;
+    // ::close(wakeup_fd_);
 }
+
+// void EventLoop::wakeup(){
+//     uint64_t one = 1;
+//     ssize_t n = ::write(wakeup_fd_, &one, sizeof one);
+//     if (n != sizeof one) {
+//         std::cerr << "EventLoop::wakeup() writes " << n << " bytes instead of 8" << std::endl;
+//     }
+// }
+// void EventLoop::handle_read() {
+//     uint64_t one = 1;
+//     ssize_t n = ::read(wakeup_fd_, &one, sizeof one);
+//     if (n != sizeof one) {
+//         std::cerr << "EventLoop::handle_read() reads " << n << " bytes instead of 8" << std::endl;
+//     }
+// }
+// void EventLoop::runInLoop(Functor cb){
+//     if(isInLoopThread()){
+//         cb();
+//     }else{
+//         queueInLoop(std::move(cb));
+//     }
+// }   
+// void EventLoop::queueInLoop(Functor cb){
+//     {
+//         std::lock_guard<std::mutex> lock(mutex_);
+//         pending_functors_.emplace_back(std::move(cb));
+//     }
+
+//     if(!isInLoopThread() || calling_pending_functors_){
+//         wakeup();
+//     }
+// }
 void EventLoop::loop() {
     while (true) {
         std::vector<Channel *> active_channels;
         active_channels.clear();
-        server_epoller->wait(0, active_channels);
+        server_epoller->wait(10000, active_channels);
         for (auto channel : active_channels) {
             channel->handle_event();
         }
--- EventLoop.h
+++ EventLoop.h
@@ -4,20 +4,42 @@
 #include <functional>
 #include "Epoller.h"
 #include <memory> // for unique_ptr
+        #include "thread"
+#include "mutex"
 class Epoller;
 class Channel;
 class EventLoop {
 public:
+    //using Functor = std::function<void()>;
     EventLoop();
     ~EventLoop();
     void loop();
+    //void quit();
+
+
+    //void wakeup();
+    //void runInLoop(Functor cb);
+    //void queueInLoop(Functor cb);
 
     void addChannel(Channel *channel);
     void modChannel(Channel *channel);
     void delChannel(Channel *channel);
-    //Epoller get_server_epoller() { return server_epoller; }
+    //bool isInLoopThread() const { return thread_id_ == std::this_thread::get_id(); }
 private:
+    //void handle_read();         // 读门铃
+   // void doPendingFunctors();   // 读任务队列
+
+    //const std::thread::id thread_id_;
+
+    //bool looping_;
+    //bool quit_;
+
+    //int wakeup_fd_;
+    //std::unique_ptr<Channel> wakeup_channel_;
+
     std::unique_ptr<Epoller> server_epoller;
-    //Epoller server_epoller;
-    //Channel *server_channel;
+    
+    //bool calling_pending_functors_;         // 防止死锁
+    //std::vector<Functor> pending_functors_; // 信箱
+    //std::mutex mutex_;
 };
